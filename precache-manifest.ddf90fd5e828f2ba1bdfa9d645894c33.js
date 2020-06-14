a32887a585e3f14ea90c68ffc8905f9da3b9156f
Rajeev Rajchal <kdark669@gmail.com>
2020-06-14 07:37:25 +0545

    Updates

---
+++ precache-manifest.ddf90fd5e828f2ba1bdfa9d645894c33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09881a0cebbed3e878c75a2de048cc64",
+    "url": "/furniture/index.html"
+  },
+  {
+    "revision": "8264943a2773264c523b",
+    "url": "/furniture/static/css/main.f5d883a9.chunk.css"
+  },
+  {
+    "revision": "037d8fc90156236f0e19",
+    "url": "/furniture/static/js/2.82821282.chunk.js"
+  },
+  {
+    "revision": "8264943a2773264c523b",
+    "url": "/furniture/static/js/main.2f7c34a8.chunk.js"
+  },
+  {
+    "revision": "57d280444de2cffe04b5",
+    "url": "/furniture/static/js/runtime~main.2ead7011.js"
+  },
+  {
+    "revision": "8c25d4e66e6715686fafb35c3677d682",
+    "url": "/furniture/static/media/banner.8c25d4e6.jpg"
+  },
+  {
+    "revision": "6a3cfd662886e7b9930a279f36c58a7e",
+    "url": "/furniture/static/media/footer.6a3cfd66.webp"
+  },
+  {
+    "revision": "3e672f2dc48a317f8b887a6ae70059c9",
+    "url": "/furniture/static/media/furniture.3e672f2d.jpg"
+  }
+]);
